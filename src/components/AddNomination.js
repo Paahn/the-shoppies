2456da26400bf3d60a5c0fed09f02331087969bc
Paahn <paahndev@gmail.com>
2021-05-05 15:02:34 -0700

    used MUI api docs to change color of icon

--- src/components/AddNomination.js
+++ src/components/AddNomination.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
+import '../styles/addnomination.css';
 
 const AddNomination = () => {
     return (
         <div>
-            <span><EmojiEventsIcon /></span>
+            <span><EmojiEventsIcon color='primary' />Nominate</span>
         </div>
     )
 }
