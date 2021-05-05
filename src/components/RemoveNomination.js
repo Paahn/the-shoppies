651a0cade8ac778f150ee8e24e8a606fd02fb772
Paahn <paahndev@gmail.com>
2021-05-05 15:32:24 -0700

    Create RemoveNomination.js

---
+++ src/components/RemoveNomination.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HighlightOffIcon from '@material-ui/icons/HighlightOff';
+
+const RemoveNomination = () => {
+    return (
+        <div>
+            <span><HighlightOffIcon /></span>
+        </div>
+    )
+}
+
+export default RemoveNomination
