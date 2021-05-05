3f347749c42ab349487e655d99747be5f2392c21
Paahn <paahndev@gmail.com>
2021-05-05 14:52:55 -0700

    create AddNomination component to nominate a movie

---
+++ src/components/AddNomination.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmojiEventsIcon from '@material-ui/icons/EmojiEvents';
+
+const AddNomination = () => {
+    return (
+        <div>
+            <span><EmojiEventsIcon /></span>
+        </div>
+    )
+}
+
+export default AddNomination
