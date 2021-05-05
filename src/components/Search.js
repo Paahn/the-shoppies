3426578bb9dc1397ddbaaa63a1b47d2e4271f1d3
Paahn <paahndev@gmail.com>
2021-05-05 15:04:03 -0700

    edited double quotes to single

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
-import IconButton from "@material-ui/core/IconButton";
-import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 const Search = ({ inputMovies, setInputMovies }) => {
     const searchMovies = (event) => {
