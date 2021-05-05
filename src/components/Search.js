df9eb48b867bfe68c77a4acc259dabeab67203af
Paahn <paahndev@gmail.com>
2021-05-05 14:36:29 -0700

    deleted dead code

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,6 @@ import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import InputLabel from '@material-ui/core/InputLabel';
 
 const Search = ({ inputMovies, setInputMovies }) => {
     const searchMovies = (event) => {
