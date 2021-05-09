7e346216c539b8f37d570e87be9870cd1fb0ef47
Paahn <paahndev@gmail.com>
2021-05-08 21:19:35 -0700

    made searchbar take up 100% of its parent container

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,6 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import '../styles/search.css';
 
 const Search = ({ inputMovies, setInputMovies }) => {
     const searchMovies = (event) => {
@@ -11,6 +12,7 @@ const Search = ({ inputMovies, setInputMovies }) => {
     return (
         <div>
             <TextField
+              className='search-bar'
               InputProps={{
                 startAdornment: (
                   <InputAdornment position='start'>
